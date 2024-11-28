4693d37271ba1322cae5ff41914014562d9460ea
lapshuntanya <lapshun.tatiana@gmail.com>
2024-11-28 17:01:42 +0200

    main

--- P34-T36-stack/P34-T36-stack.cpp
+++ P34-T36-stack/P34-T36-stack.cpp
@@ -1,6 +1,29 @@
-#include <iostream>
+#include "MyStack.h"
+#include <string>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    myStack::Stack<std::string> p34;
+    p34.push_back("Students");
+    p34.push_back("of");
+    p34.push_back("the");
+    p34.push_back("group");
+    p34.push_back("P34");
+    p34.showStack(); //"Students", "of", "the", "group", "P34"
+    cout << endl;
+
+    p34.pop_back();
+    p34.showStack();//"Students", "of", "the", "group"
+
+    cout << "--------------------------------\n\n";
+    //Розвантаження стеку
+    while (!p34.isEmpty()) {
+         
+        if (p34.top().size() == 2)
+            cout << p34.top() << " ";
+
+        p34.pop_back();
+    }
+    cout << endl;
+    p34.showStack();
 }
